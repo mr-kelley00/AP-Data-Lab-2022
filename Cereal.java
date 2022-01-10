e9857c437cf1667a5037a545c40bea370e6dfdd3
Ryan Kelley <kelleyr@duvalschools.org>
2022-01-10 09:32:21 -0500

    v0.4

--- Cereal.java
+++ Cereal.java
@@ -1,1 +1,33 @@
-// Ryan Kelley, 01/10/22, AP Data Lab 2022, v0.0 
+// Ryan Kelley, 01/10/22, AP Data Lab 2022, v0.4
+
+public class Cereal 
+{
+
+    private String cerealName; 
+    private double fiber;
+    private double cups; 
+    private double fiberPerCup; 
+
+    public Cereal(String cerealName, double fiber, double cups, double fiberPerCup)
+
+    {
+     name = cerealName;
+     fiber = fiber;
+     cups = cups;
+     FPC = fiberPerCup;
+
+    }
+
+    public Cereal() 
+    {
+        name = "";
+        fiber = 0.0;
+        cups = 0.0;
+        FPC = 0.0; 
+
+    }
+
+
+    
+
+}
